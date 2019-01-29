16aa83b9080e9c2acc3a4b899a9e332eb8a098a9
Saint-Jean Germila <germila@noukod.com>
2019-01-29 19:53:34 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,24 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
 
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+  end
+
+  def new
   end
 
   def create
-    Post.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to posts_path
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,3 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 end
-end
