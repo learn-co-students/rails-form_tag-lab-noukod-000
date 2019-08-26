48605d89327aeb74c8c3304f45fb89f10e29c337
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-26 15:43:19 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,17 @@
 class StudentsController < ApplicationController
   
-  def index
+  def index #students_path
     @students = Student.all
   end
 
-  def show
+  def show #student_path
     @student = Student.find(params[:id])
   end
 
-  def new
+  def new #student_new_path
   end
 
-  def create
+  def create #students_path
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path
   end
